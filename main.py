da05cc7d520b6908834ab8e69697b5f7ffb3142d
AIindas <william.dillon@outlook.com>
2026-07-31 18:58:48 +0100

    added a Vigenère cypher

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def ceaserCypher():
-    plaintext = input("input plaintext : ")
     ciphertext = []
+    plaintext = input("input plaintext : ")
     plaintext = list(plaintext.lower())
     key = int(input("shift amount : "))
     for letter in plaintext:
@@ -16,11 +16,39 @@ def ceaserCypher():
 
     print("".join(ciphertext))
 
+def VigenèreCypher():
+    ciphertext = []
+    plaintext = input("input plaintext : ")
+    plaintext = list(plaintext.lower())
+    key = input("input cypher key : ")
+    key_index = 0
+    for letter in plaintext:
+        if letter in alphabet:
+            pos = alphabet.index(letter)
+            keypos = alphabet.index(key[key_index % len(key)])
+            newpos = (pos + keypos) % 26
+            ciphertext.append(alphabet[newpos])
+            key_index += 1
+        else:
+            ciphertext.append(letter)
+
+    print("".join(ciphertext))
+
+
+
+
+
+
+
+
+
 def main():
-    choice = int(input("Which Cypher methord would you like to use \n[1] Ceaser Cypher \n"))
+    choice = int(input("Which Cypher methord would you like to use \n[1] Ceaser Cypher \n[2] Vigenère Cypher \n"))
     match choice:
         case 1:
             ceaserCypher()
+        case 2:
+            VigenèreCypher()
 
 if __name__ == '__main__':
     main()
